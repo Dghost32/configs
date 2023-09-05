1b2fd878de0c585dd0551d6574825985a839971e
Carlos Jiménez <cajimenezca@unal.edu.co>
2023-09-05 18:37:49 -0500

    ✨ New updates

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,7 +26,7 @@ lsp.format_on_save({
     ['rust_analyzer'] = { 'rust' },
     ['tsserver'] = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
     ['pyright'] = { 'python' },
-    ['clang'] = { 'c', 'cpp' }
+    ['clangd'] = { 'c', 'cpp' }
   }
 })
 
