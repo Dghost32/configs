22877522ca7956040ba2a9da426d78ea9aa4674a
Carlos Jiménez <cajimenezca@unal.edu.co>
2023-11-27 15:45:24 -0500

    ✨ Added wezterm conf

---
+++ terminal/wezterm/.wezterm.lua
@@ -0,0 +1,42 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- NOTE: your configs down here...
+
+config.color_scheme = 'Tokyo Night'
+config.window_background_opacity = 0.9
+
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font = wezterm.font {
+  family = 'JetBrains Mono',
+  harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
+}
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.inactive_pane_hsb = {
+  -- NOTE: these values are multipliers, applied on normal pane values
+  saturation = 0.9,
+  brightness = 0.8,
+}
+
+config.automatically_reload_config = true
+
+-- and finally, return the configuration to wezterm
+return config
